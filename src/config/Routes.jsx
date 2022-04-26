84c0073cb4a166e83b48a3e865393a1e22bc88cd
Nuzair Ibrahim <nuzairb@gmial.com>
2022-04-26 09:52:12 +0500

    init

---
+++ src/config/Routes.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import Home from '../pages/Home';
+import Catelog from '../pages/Catelog';
+import Detail from '../pages/Detail';
+
+const Routes = () => {
+    return (
+        <Switch>
+            <Route
+                path='/:category/search/:keyword'
+                component={Catelog}
+            />
+            <Route
+                path='/:category/:id'
+                component={Detail}
+            />
+            <Route
+                path='/:category'
+                component={Catelog}
+            />
+            <Route
+                path='/'
+                exact
+                component={Home}
+            />
+        </Switch>
+    );
+}
+
+export default Routes;
